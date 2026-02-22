6c7fa485e54a999030cd57e8395c90ffa6dee2f6
moyanoagustin <moyanoagustin025@gmail.com>
2026-02-22 12:56:28 -0300

    chore: add backend folder

---
+++ backend/bookroad/src/main/java/com/acgroup/bookroad/BookroadApplication.java
@@ -0,0 +1,13 @@
+package com.acgroup.bookroad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookroadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookroadApplication.class, args);
+	}
+
+}
